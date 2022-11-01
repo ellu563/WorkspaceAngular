f503a0cb7d8d4c05f6542c9f6e3a27d7fecac2b0
welu563 <ellu__1992@hotmail.com>
2022-11-01 10:46:29 +0200

    eka committi

---
+++ frontendtyokalut/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent,
+    FeedbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
