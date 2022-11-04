7350fda2337ebdbf50cee0c8d9a5b1dc276d2055
welu563 <ellu__1992@hotmail.com>
2022-11-04 14:10:53 +0200

    toka

--- frontendtyokalut/src/app/app.module.ts
+++ frontendtyokalut/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    FeedbackComponent
+    FeedbackComponent,
+    HelloworldComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatSliderModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontendtyokalut/src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-helloworld',
+  templateUrl: './helloworld.component.html',
+  styleUrls: ['./helloworld.component.css']
+})
+export class HelloworldComponent implements OnInit {
+
+  pagetitle: string = "Harjoittelua";
+  itemImageUrl = 'https://cdn.pixabay.com/photo/2022/09/29/17/15/halloween-7487706_960_720.jpg';
+  first_name = "Angular";
+
+  changeImg() {
+    console.log(this.first_name);
+    if (this.itemImageUrl == 'https://cdn.pixabay.com/photo/2019/10/05/19/40/pumpkins-4528653_960_720.jpg') {
+      this.itemImageUrl = 'https://cdn.pixabay.com/photo/2022/09/29/17/15/halloween-7487706_960_720.jpg';
+    } else {
+      this.itemImageUrl = 'https://cdn.pixabay.com/photo/2019/10/05/19/40/pumpkins-4528653_960_720.jpg'
+    }
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
