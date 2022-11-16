675d26bb85cddfde16ee3a4701136a70de715d3d
welu563 <ellu__1992@hotmail.com>
2022-11-16 17:32:15 +0200

    feedback ja toolbar muokkausta

--- frontendtyokalut/src/app/app-routing.module.ts
+++ frontendtyokalut/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/calculator', pathMatch: 'full' },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'feedback', component: FeedbackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontendtyokalut/src/app/app.module.ts
+++ frontendtyokalut/src/app/app.module.ts
@@ -11,13 +11,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     FeedbackComponent,
-    HelloworldComponent
+    HelloworldComponent,
+    ToolbarComponent
   ],
   imports: [
     AppRoutingModule,
@@ -26,7 +31,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatButtonModule,
     MatSliderModule,
     FormsModule,
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
